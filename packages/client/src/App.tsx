9be9ad18b25051d009a88794c59cd60b68fa99de
CCP Legolas <toniya@ccpgames.com>
2024-12-13 16:13:19 +0000

    basic UI change

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,9 @@
-
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useComponentValue } from "@latticexyz/react";
 import { useMUD } from "./mud/customWalletClient";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
 import { increment } from "./mud/createSystemCalls";
-
+import './App.css'; 
 export const App = () => {
   const { network, walletClient } = useMUD();
 
@@ -12,21 +11,21 @@ export const App = () => {
   const counter = network.useStore((state) => state.getValue(network.tables.Counter, {}));
 
   return (
-    <>
-    <div className="connect-button-container">
+    <div className="app-container">
+      <div className="connect-button-container">
         <ConnectButton />
       </div>
-      <div>
-        Counter:
-        <span>{counter?.value ?? "??"}</span>
+      <div className="counter-container">
+        <h2>Counter:</h2>
+        <span className="counter-value">{counter?.value ?? "??"}</span>
       </div>
-      <div>
+      <div className="increment-button-container">
         {walletClient && (
-          <button type="button" onClick={() => increment(walletClient, network)}>
+          <button className="increment-button" type="button" onClick={() => increment(walletClient, network)}>
             Increment
           </button>
         )}
       </div>
-    </>
+    </div>
   );
 };
